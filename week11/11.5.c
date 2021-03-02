c5d6509f70069b919bff8bc24e838f122cc114af
Lê Minh Đức <57053021+lmd-278@users.noreply.github.com>
2021-03-02 17:03:07 +0700

    Add files via upload

--- week11/11.5.c
+++ week11/11.5.c
@@ -3,57 +3,54 @@
 void cycle1(int *x, int *y, int *z);
 void cycle2(int **x, int **y, int **z);
 
-int main(void)
-{
-    int x, y, z, option;
-    printf("Enter 3 numbers:\n");
-    printf("x = ");
-    scanf("%d", &x);
-    printf("y = ");
-    scanf("%d", &y);
-    printf("z = ");
-    scanf("%d", &z);
-    printf("\n");
-    int *p = &x, *q = &y, *r = &z;
+int main(void) {
+	int x, y, z, option;
+	printf("Enter 3 numbers:\n");
+	printf("x = ");
+	scanf("%d", &x);
+	printf("y = ");
+	scanf("%d", &y);
+	printf("z = ");
+	scanf("%d", &z);
+	printf("\n");
+	int *p = &x, *q = &y, *r = &z;
 
-    printf("1. Print with labels the values of x, y, z, p, q, r, *p, *q, *r\n");
-    printf("2. Rotate values of x, y, z. Print with labels the values of x, y, z, p, q, r, *p, *q, *r\n");
-    printf("3. Rotate values of p, q, r. Print with labels the values of x, y, z, p, q, r, *p, *q, *r\n");
-    printf("Enter your option: ");
-    scanf("%d", &option);
-    printf("\n");
-    if (option == 1);
-    else if (option == 2)
-        cycle1(&x, &y, &z);
-    else if (option == 3)
-        cycle2(&p, &q, &r);
-    else
-    {
-        printf("Invalid option\n");
-        return 1;
-    }
+	printf("1. Print with labels the values of x, y, z, p, q, r, *p, *q, *r\n");
+	printf("2. Rotate values of x, y, z. Print with labels the values of x, y, z, p, q, r, *p, *q, *r\n");
+	printf("3. Rotate values of p, q, r. Print with labels the values of x, y, z, p, q, r, *p, *q, *r\n");
+	printf("\n");
 
-    printf("x = %d, y = %d, z = %d\n", x, y, z);
-    printf("p = %p, q = %p, r = %p\n", p, q, r);
-    printf("*p = %d, *q = %d, *r = %d\n", *p, *q, *r);
+	printf("x = %d, y = %d, z = %d\n", x, y, z);
+	printf("p = %x, q = %x, r = %x\n", p, q, r);
+	printf("*p = %d, *q = %d, *r = %d\n", *p, *q, *r);
+	printf("\n");
 
-    return 0;
+	cycle1(&x, &y, &z);
+	printf("x = %d, y = %d, z = %d\n", x, y, z);
+	printf("p = %x, q = %x, r = %x\n", p, q, r);
+	printf("*p = %d, *q = %d, *r = %d\n", *p, *q, *r);
+	printf("\n");
+
+	cycle2(&p, &q, &r);
+	printf("x = %d, y = %d, z = %d\n", x, y, z);
+	printf("p = %x, q = %x, r = %x\n", p, q, r);
+	printf("*p = %d, *q = %d, *r = %d\n", *p, *q, *r);
+
+	return 0;
 }
 
-void cycle1(int *x, int *y, int *z)
-{
-    int tmp;
-    tmp = *x;
-    *x = *y;
-    *y = *z;
-    *z = tmp;
+void cycle1(int *x, int *y, int *z) {
+	int tmp;
+	tmp = *x;
+	*x = *y;
+	*y = *z;
+	*z = tmp;
 }
 
-void cycle2(int **x, int **y, int **z)
-{
-    int *tmp;
-    tmp = *x;
-    *x = *y;
-    *y = *z;
-    *z = tmp;
+void cycle2(int **x, int **y, int **z) {
+	int *tmp;
+	tmp = *x;
+	*x = *y;
+	*y = *z;
+	*z = tmp;
 }
